b08a56b65408b47f3383552155d3cc6e3e931c54
Jackson West <jgw@amazon.com>
2021-03-10 16:24:48 -0600

    external-attacher attribution generation

--- projects/kubernetes-csi/external-attacher/build/create_attribution.sh
+++ projects/kubernetes-csi/external-attacher/build/create_attribution.sh
@@ -13,45 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-set -x
 set -o errexit
 set -o nounset
 set -o pipefail
 
-REPO="$1"
-CLONE_URL="$2"
-TAG="$3"
-GOLANG_VERSION="$4"
-BIN_ROOT="_output/bin"
-BIN_PATH=$BIN_ROOT/$REPO
-
-readonly SUPPORTED_PLATFORMS=(
-  linux/amd64
-  linux/arm64
-)
-
 MAKE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 source "${MAKE_ROOT}/../../../build/lib/common.sh"
 
-function build::external-attacher::binaries(){
-  mkdir -p $BIN_PATH
-  git clone $CLONE_URL $REPO
-  cd $REPO
-  git checkout $TAG
-  build::common::use_go_version $GOLANG_VERSION
-  for platform in "${SUPPORTED_PLATFORMS[@]}";
-  do
-    OS="$(cut -d '/' -f1 <<< ${platform})"
-    ARCH="$(cut -d '/' -f2 <<< ${platform})"
-    make BUILD_PLATFORMS="$OS $ARCH"
-    mkdir -p ../${BIN_PATH}/${OS}-${ARCH}
-    mv bin/* ../${BIN_PATH}/${OS}-${ARCH}
-    make clean
-  done
-  build::gather_licenses ./ $MAKE_ROOT/_output "./cmd/csi-attacher"
-  cd ..
-  rm -rf $REPO
-}
+CLONE_URL="$1"
+GOLANG_VERSION="$2"
 
-build::external-attacher::binaries
+build::common::use_go_version $GOLANG_VERSION
+
+build::generate_attribution $CLONE_URL
