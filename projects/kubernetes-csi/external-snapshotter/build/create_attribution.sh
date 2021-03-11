a51d50127caa1b42577a0f4a3f7cc99225b8a1b5
Jackson West <jgw@amazon.com>
2021-03-10 19:05:22 -0600

    external snapshotter attribution generation

--- projects/kubernetes-csi/external-snapshotter/build/create_attribution.sh
+++ projects/kubernetes-csi/external-snapshotter/build/create_attribution.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Copyright 2020 Amazon.com Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,13 +13,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ARG BASE_IMAGE
-FROM $BASE_IMAGE
+set -o errexit
+set -o nounset
+set -o pipefail
 
-ARG TARGETARCH
-ARG TARGETOS
+MAKE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+source "${MAKE_ROOT}/../../../build/lib/common.sh"
 
-COPY _output/LICENSES /LICENSES
-COPY ATTRIBUTION.txt /ATTRIBUTION.txt
-COPY _output/bin/external-snapshotter/$TARGETOS-$TARGETARCH/csi-snapshotter /csi-snapshotter
-ENTRYPOINT ["/csi-snapshotter"]
+REPO="$1"
+GOLANG_VERSION="$2"
+
+build::common::use_go_version $GOLANG_VERSION
+
+MODULE_NAME=$(cd $REPO && go mod edit -json | jq -r '.Module.Path')
+
+build::generate_attribution $MODULE_NAME
+build::diff_attribution "${MAKE_ROOT}/ATTRIBUTION.txt" "${MAKE_ROOT}/_output/attribution/ATTRIBUTION.txt"
